a053115cd1d0bec610c123b2268a701a8d412689
Dylan <57853417+4812571@users.noreply.github.com>
2022-10-10 21:12:23 -0400

    more tests!

--- mayhem/test_fuzz.py
+++ mayhem/test_fuzz.py
@@ -7,9 +7,15 @@ with atheris.instrument_imports():
 
 def testOneInput(data):
     fdp = atheris.FuzzedDataProvider(data)
+    num = fdp.ConsumeIntInRange(0, 1)
 
-    input = fdp.ConsumeUnicodeNoSurrogates(64)
-    json2html.convert(json = input)
+    if num == 0:
+        in_str_1 = fdp.ConsumeUnicodeNoSurrogates(1024)
+        json2html.convert(json=in_str_1)
+    if num == 1:
+        in_str_1 = fdp.ConsumeUnicodeNoSurrogates(1024)
+        in_str_2 = fdp.ConsumeUnicodeNoSurrogates(1024)
+        json2html.convert(json=in_str_1, table_attributes=in_str_2)
         
 def main():
     atheris.Setup(sys.argv, testOneInput)
